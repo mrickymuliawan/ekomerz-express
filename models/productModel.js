e8c46eba557556844439176f3a65bbbe3908858d
Ricky Muli <mrickymuliawan@gmail.com>
2021-05-06 22:08:35 +0700

    crud api complete

--- models/productModel.js
+++ models/productModel.js
@@ -1,25 +1,56 @@
 const db = require('../connection')
+var moment = require('moment');
 
 const get = (cb) => {
-  const sql = 'select * from products'
-  db.query(sql, (err, res) => {
-    cb(res)
+  const sql = 'select * from products where deleted_at is NULL'
+  db.query(sql, (err, result) => {
+    cb(result)
+  })
+}
+
+const getArchived = (cb) => {
+  const sql = 'select * from products where deleted_at is not NULL'
+  db.query(sql, (err, result) => {
+    cb(result)
   })
 }
 
 const getSingle = (id, cb) => {
   const sql = `select * from products where id=${id}`
-  db.query(sql, (err, res) => {
-    cb(res)
+  db.query(sql, (err, result) => {
+    cb(result)
   })
 }
 
 const create = (body, cb) => {
   const sql = `insert into products (name, base_price, price, description) values ('${body.name}', ${body.base_price}, ${body.price}, '${body.description}')`
 
-  db.query(sql, (err, res) => {
-    cb(res)
+  db.query(sql, (err, result) => {
+    cb(result)
   })
 }
 
-module.exports = { get, getSingle, create }
+const update = (id, body, cb) => {
+  const sql = `update products set name='${body.name}', base_price=${body.base_price}, price=${body.price}, description='${body.description}' where id=${id}`
+
+  db.query(sql, (err, result) => {
+    cb(result)
+  })
+}
+
+const deleteData = (id, cb) => {
+  const now = moment().format('YYYY-MM-DD HH:mm:s')
+  const sql = `update products set deleted_at='${now}' where id=${id}`
+  db.query(sql, (err, result) => {
+    cb(result)
+  })
+}
+
+const restore = (id, cb) => {
+  const sql = `update products set deleted_at=NULL where id=${id}`
+  db.query(sql, (err, result) => {
+    cb(result)
+  })
+}
+
+module.exports = { get, getSingle, create, update, deleteData, getArchived, restore }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,19 +9,26 @@ const productModel = require('../models/productModel')
 // delete /products/1
 
 router.get('/products', (req, res) => {
-  productModel.get((products) => {
-    return res.json(products)
+  productModel.get((result) => {
+    res.json(result)
+  })
+})
+
+
+router.get('/archived-products', (req, res) => {
+  productModel.getArchived((result) => {
+    res.json(result)
   })
 })
 
 router.get('/products/:id', (req, res) => {
-  productModel.getSingle(req.params.id, (products) => {
-    return res.json(products[0])
+  productModel.getSingle(req.params.id, (result) => {
+    return res.json(result[0])
   })
 })
 
 router.post('/products', (req, res) => {
-  productModel.create(req.body, (data) => {
+  productModel.create(req.body, (result) => {
     return res.json({
       message: 'Created success'
     })
@@ -29,18 +36,27 @@ router.post('/products', (req, res) => {
 })
 
 router.put('/products/:id', (req, res) => {
-  // update
-  return res.json({
-    message: 'success updated',
-    product: {
-
-    }
+  productModel.update(req.params.id, req.body, (result) => {
+    return res.json({
+      message: 'success updated',
+    })
   })
+
 })
 
 router.delete('/products/:id', (req, res) => {
-  return res.json({
-    message: 'success deleted',
+  productModel.deleteData(req.params.id, (result) => {
+    return res.json({
+      message: 'success deleted',
+    })
+  })
+})
+
+router.put('/products/:id/restore', (req, res) => {
+  productModel.restore(req.params.id, (result) => {
+    return res.json({
+      message: `products with ID ${req.params.id} restored`
+    })
   })
 })
 module.exports = router
